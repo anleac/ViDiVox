d3b20bdf2a65c1ff0300149d5fdabdd74e6d1eb9
Nick <npol807@aucklanduni.ac.nz>
2015-09-25 14:51:26 +1200

    Commenting "addAudio" swingworker class

--- src/tools/addAudio.java
+++ src/tools/addAudio.java
@@ -6,6 +6,9 @@ import javax.swing.SwingWorker;
 
 import frames.ProgressBarFrame;
 
+/*
+ * Swingworker extension for running an ffmpeg command in another thread outside the EDT
+ */
 public class addAudio extends SwingWorker<Void, Void>{
 	String mp3Path;
 	String videoPath;
@@ -21,21 +24,25 @@ public class addAudio extends SwingWorker<Void, Void>{
 	}
 	
 	
+	//Overriding the doinbackground method for ffmpeg command to do in background
 	@Override
 	protected Void doInBackground() throws Exception {
 		
 		if (!Tools.hasExtension(outVidPath)){
 			outVidPath += ".avi";
 		}
+		//This is the ffmpeg command used to apply selected audio to current video
 		String cmd = "ffmpeg -i " + videoPath + " -i " + mp3Path + " -map 0:v -map 1:a " + outVidPath;
+		
+		
 		ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c", cmd);
 
 		try {
 			
-			
+			//Running process
 			Process process = pb.start();
 			process.waitFor();
-			
+			//Waiting for process to end
 			
 		} catch (Exception e) {
 			Tools.displayError("Error adding audio to video");
@@ -43,7 +50,9 @@ public class addAudio extends SwingWorker<Void, Void>{
 		return null;
 	}
 	protected void done(){
+		//Close the "working..." frame 
 		ProgressBarFrame.pbFrame.setVisible(false);
+		//Displaying success and path in a pop up message upon completion
 		Tools.displayInfo("Video succesfully saved to\n" + outVidPath);
 	}
 	
